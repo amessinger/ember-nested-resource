1babb5c9285f4eef1c23fb8768004af6a9aa320f
Antonin Messinger <antonin.messinger@gmail.com>
2019-03-13 12:33:19 +0100

    use api for posts

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,20 +1,7 @@
 import Route from '@ember/routing/route';
-import { A } from '@ember/array';
-import EmberObject from '@ember/object';
 
 export default Route.extend({
   model() {
-    return A([
-      EmberObject.create({
-        id: "1",
-        title: 'First Post',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      }),
-      EmberObject.create({
-        id: "2",
-        title: 'Second Post',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      })
-    ]);
+    return this.store.findAll('post');
   }
 });
